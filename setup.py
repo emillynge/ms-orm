3d9e981ed239bee323b103a2579bc56870cb456f
emillynge <emillynge24@gmail.com>
2017-08-30 19:56:12 +0200

    Adding git versioning

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 from setuptools import setup
+import versioneer
 
 setup(
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     name='ms-orm',
-    version='1.1.0',
     packages=['msorm'],
     url='https://github.com/emillynge/ms-orm',
     install_requires=[
         'keyring>=10.2',
         'aioxmlrpc',
         'aiohttp',
-
+        'json_tricks'
     ],
     license='Apache',
     author='emillynge',
